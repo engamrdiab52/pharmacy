a3cb46093038b01e704703cf7b9eebb962ec372c
engam <engamrdiab52@gmail.com>
2022-03-07 19:33:46 +0200

    status bar

--- app/src/main/java/com/amrabdelhamiddiab/pharmacy_1/SplashActivity.kt
+++ app/src/main/java/com/amrabdelhamiddiab/pharmacy_1/SplashActivity.kt
@@ -1,10 +1,31 @@
 package com.amrabdelhamiddiab.pharmacy_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    private val runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
     }
 }
